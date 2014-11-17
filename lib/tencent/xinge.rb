66220e361fc7883e8ffca8b1e930ab3681b834e4
yuyu <datty258@126.com>
2014-11-17 11:21:26 +0800

    commit

---
+++ lib/tencent/xinge.rb
@@ -0,0 +1,11 @@
+require "tencent/xinge/version"
+
+module Tencent
+  module Xinge
+    class Sender
+    	def self.test(test)
+	    	puts test
+	    end
+    end
+  end
+end
---
+++ lib/tencent/xinge/version.rb
@@ -0,0 +1,5 @@
+module Tencent
+  module Xinge
+    VERSION = "0.0.1"
+  end
+end
